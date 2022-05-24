3478d7e6ac3fbca74c42e02244056a75c414d1f9
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-05-24 13:36:40 +0700

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,24 +11,25 @@ class HomeController extends Controller
     public function index()
     {
         $responses = Http::get('https://isb.lkpp.go.id/isb/api/1683a6a8-32b4-40f9-a9be-dd07e8942ef3/json/736987856/PaketAnggaranPenyedia1618/tipe/4:12/parameter/2022:D129');
-        dd($responses[0]);
         return $responses->json();
     }
     public function store()
     {
         $responses = Http::get('https://isb.lkpp.go.id/isb/api/1683a6a8-32b4-40f9-a9be-dd07e8942ef3/json/736987856/PaketAnggaranPenyedia1618/tipe/4:12/parameter/2022:D129');
-        // $data = $request->only('koderup','id_rup_client','kodekomponen','kodekegiatan','pagu','mak','sumberdana','kodeobjekakun','tahun_anggaran_dana');
-        PaketAnggaranPenyedia::create([
-            'koderup' => $responses[0]['koderup'],
-            'id_rup_client' => $responses[0]['id_rup_client'],
-            'kodekomponen' => $responses[0]['kodekomponen'],
-            'kodekegiatan' => $responses[0]['kodekegiatan'],
-            'pagu' => $responses[0]['pagu'],
-            'mak' => $responses[0]['mak'],
-            'sumberdana' => $responses[0]['sumberdana'],
-            'kodeobjekakun' => $responses[0]['kodeobjekakun'],
-            'tahun_anggaran_dana' => $responses[0]['tahun_anggaran_dana']
-        ]);
+        PaketAnggaranPenyedia::truncate();
+        foreach (json_decode($responses) as $response) {
+            PaketAnggaranPenyedia::create([
+                'koderup' => $response->koderup,
+                'id_rup_client' => $response->id_rup_client,
+                'kodekomponen' => $response->kodekomponen,
+                'kodekegiatan' => $response->kodekegiatan,
+                'pagu' => $response->pagu,
+                'mak' => $response->mak,
+                'sumberdana' => $response->sumberdana,
+                'kodeobjekakun' => $response->kodeobjekakun,
+                'tahun_anggaran_dana' => $response->tahun_anggaran_dana
+            ]);
+        }
         return 'sukses';
     }
 }
