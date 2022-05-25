1573d5506f82b3b400989c9672ec1e6e3d435fae
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-05-25 07:06:48 +0700

    dd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,12 +17,12 @@ class HomeController extends Controller
     }
     public function store_paket_anggaran_penyedia(Request $request)
     {
-        $responses = Http::acceptJson()->get('https://isb.lkpp.go.id/isb/api/1683a6a8-32b4-40f9-a9be-dd07e8942ef3/json/736987856/PaketAnggaranPenyedia1618/tipe/4:12/parameter/' . $request['year'] . ':D129');
+        $responses = Http::get('https://isb.lkpp.go.id/isb/api/1683a6a8-32b4-40f9-a9be-dd07e8942ef3/json/736987856/PaketAnggaranPenyedia1618/tipe/4:12/parameter/' . $request['year'] . ':D129');
+        dd($responses);
         $anggarans = PaketAnggaranPenyedia::where('tahun_anggaran_dana', $request['year'])->get();
         foreach ($anggarans as $anggaran) {
             $anggaran->delete();
         }
-        dd(json_decode($responses));
         foreach (json_decode($responses) as $response) {
             PaketAnggaranPenyedia::create([
                 'koderup' => $response->koderup,
