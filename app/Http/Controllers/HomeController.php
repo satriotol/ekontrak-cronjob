1de88d5d8a31c9871f43787394c3bfdf2a540626
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-05-25 07:09:36 +0700

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,8 +17,9 @@ class HomeController extends Controller
     }
     public function store_paket_anggaran_penyedia(Request $request)
     {
+        dd($request->year, $request['year']);
         $responses = Http::get('https://isb.lkpp.go.id/isb/api/1683a6a8-32b4-40f9-a9be-dd07e8942ef3/json/736987856/PaketAnggaranPenyedia1618/tipe/4:12/parameter/' . $request['year'] . ':D129');
-        return $responses;
+        // return $responses;
         $anggarans = PaketAnggaranPenyedia::where('tahun_anggaran_dana', $request['year'])->get();
         foreach ($anggarans as $anggaran) {
             $anggaran->delete();
