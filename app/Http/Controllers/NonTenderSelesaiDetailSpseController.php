4bcdc87411eb63ec6f6a68b0e3e9e32b72434230
Satrio Jati Wicaksono <48653478+satriotol@users.noreply.github.com>
2022-12-07 12:20:17 +0700

    fixing

--- app/Http/Controllers/NonTenderSelesaiDetailSpseController.php
+++ app/Http/Controllers/NonTenderSelesaiDetailSpseController.php
@@ -18,16 +18,10 @@ class NonTenderSelesaiDetailSpseController extends Controller
     {
         $url = 'https://inaproc.lkpp.go.id/isb/api/eadf3b47-5899-4f7a-8229-d9aae56af726/json/736987899/NonTenderSelesaiDetailSPSE/tipe/4:4/parameter/' . $year . ':108';
         $responses = Http::get($url);
-        $anggarans = NonTenderSelesaiDetailSpse::where('tahun_anggaran', $year)->get();
-        if ($anggarans->count() > 0) {
-            foreach ($anggarans as $anggaran) {
-                $anggaran->delete();
-            }
-        }
         foreach ($responses->json() as $response) {
             dispatch(new NonTenderSelesaiDetailSpseJob($response));
         }
-        return ResponseFormatter::success(NonTenderSelesaiDetailSpse::all()->count(), 'Sukses Menambah Data');
+        return ResponseFormatter::success($url, 'Sukses Menambah Data');
     }
 
     /**
